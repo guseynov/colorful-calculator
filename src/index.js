7bdb5c99c62fda68dc673539ef6dc3bde3dd15fd
Alex Guseynov <alex.guseynov.23@gmail.com>
2026-06-15 09:28:27 +0400

    init project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Calculator from "./Calculator";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <Calculator />
+  </Provider>,
+  rootElement
+);
---
+++ src/redux/actionCreators.js
@@ -0,0 +1,37 @@
+export function clear() {
+  return {
+    type: "CLEAR"
+  };
+}
+
+export function write(content) {
+  return {
+    type: "WRITE",
+    payload: content
+  };
+}
+
+export function setAction(actionName) {
+  return {
+    type: "SET_ACTION",
+    payload: actionName
+  };
+}
+
+export function equals() {
+  return {
+    type: "EQUALS"
+  };
+}
+
+export function backspace() {
+  return {
+    type: "BACKSPACE"
+  };
+}
+
+export function changeColors() {
+  return {
+    type: "CHANGE_COLORS"
+  };
+}
